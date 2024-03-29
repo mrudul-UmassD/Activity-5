d1e0776a768c0394f3c6903ad6e22af2b8ef985e
mrudul-UmassD <158495104+mrudul-UmassD@users.noreply.github.com>
2024-03-29 19:55:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const commission = require('./CalculateComission');
-const result = commission(70, 80, 90);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter the value of lock: ', (lock) => {
+    rl.question('Enter the value of stock: ', (stock) => {
+        rl.question('Enter the value of barrel: ', (barrel) => {
+            const result = commission(lock, stock, barrel);
+            console.log(result);
+            rl.close();
+        });
+    });
+});
 console.log(result);
